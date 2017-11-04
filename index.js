1515185e2d68245f14583b639d2a0b196cc6613c
Ryan Do <falcsmike7@gmail.com>
2017-11-04 12:52:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here:
+function driversWithRevenueOver(array, revenue) {
+  return array.filter(function(element) {
+    return element.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver(array, revenue) {
+  return array.filter(function(element) {
+    if (element.revenue > revenue) {
+      return element.name;
+    }
+  });
+}
